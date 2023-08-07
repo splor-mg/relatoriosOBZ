299275058d1f02baf3c7cc669fe27ea709f18fa9
Francisco Júnior <fjunior.alves.oliveira@gmail.com>
2023-08-07 14:39:15 -0300

    initial commit

---
+++ R/utils.R
@@ -0,0 +1,29 @@
+remap_unidades <- function(data, uo = NULL, acao = NULL) {
+  dt <- data.table::copy(data)
+
+  if(!is.null(uo)) {
+    data.table::setnames(dt, uo, "UO_COD")
+  }
+
+  if(!is.null(acao)) {
+    data.table::setnames(dt, acao, "ACAO_COD")
+  }
+
+  dt[
+    UO_COD == 1501 & ACAO_COD %in% c(4480, 4481, 4482), UO_COD := 1502
+  ]
+
+  dt[
+    UO_COD == 1511 & ACAO_COD %in% c(4100, 4105, 4124, 4134, 4135), UO_COD := 1551
+  ]
+
+  if(!is.null(uo)) {
+    data.table::setnames(dt, "UO_COD", uo)
+  }
+
+  if(!is.null(acao)) {
+    data.table::setnames(dt, "ACAO_COD", acao)
+  }
+
+  return(dt[])
+}
