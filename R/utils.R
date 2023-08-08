f0e43e88cac0edcc06adc431d4c60fc764864723
Francisco Júnior <fjunior.alves.oliveira@gmail.com>
2023-08-08 07:41:37 -0300

    adiciona caso de transposicao acoes pcmg para seplag em 2023

--- R/utils.R
+++ R/utils.R
@@ -13,9 +13,13 @@ remap_unidades <- function(data, uo = NULL, acao = NULL) {
     UO_COD == 1501 & ACAO_COD %in% c(4480, 4481, 4482), UO_COD := 1502
   ]
 
+
+  # em 2023 houve transposição das ações da PCMG para a SEPLAG
   dt[
-    UO_COD == 1511 & ACAO_COD %in% c(4100, 4105, 4124, 4134, 4135), UO_COD := 1551
+    (UO_COD == 1511 | 1501) & ACAO_COD %in% c(4100, 4105, 4124, 4134, 4135), UO_COD := 1551
   ]
+
+
 
   if(!is.null(uo)) {
     data.table::setnames(dt, "UO_COD", uo)
