8e372850cc2a5b5ef21378061a375283c079a7b7
Francisco Júnior <fjunior.alves.oliveira@gmail.com>
2023-08-08 08:28:27 -0300

    adiciona remap de secom e casa civil

--- R/utils.R
+++ R/utils.R
@@ -19,7 +19,25 @@ remap_unidades <- function(data, uo = NULL, acao = NULL) {
     (UO_COD == 1511 | 1501) & ACAO_COD %in% c(4100, 4105, 4124, 4134, 4135), UO_COD := 1551
   ]
 
+  dt[
+    UO_COD == 1221 & ACAO_COD == 4408, UO_COD := 1721
+  ]
 
+  dt[
+    UO_COD == 1491 & ACAO_COD == 2015, UO_COD := 1721
+  ]
+
+  dt[
+    UO_COD == 1501 & ACAO_COD == 4140, UO_COD := 1721
+  ]
+
+  dt[
+    UO_COD == 1631 & ACAO_COD %in% c(2058, 2059), UO_COD := 1711
+  ]
+
+  dt[
+    UO_COD == 1631 & ACAO_COD == 2060, UO_COD := 1491
+  ]
 
   if(!is.null(uo)) {
     data.table::setnames(dt, "UO_COD", uo)
